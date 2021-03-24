aa74da1cf91c969c6a06d472962b6e1b2fc6ed82
kg4543 <jyc351919@naver.com>
2021-03-24 14:06:36 +0900

    컨버트 & 멀티바인딩

---
+++ WPFApp/WPFAdvBank/WpfTestApp/PersonList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTestApp
+{
+    class PersonList : ObservableCollection<Person>
+    {
+        public PersonList()
+        {
+            this.Add(new Person("Willa", "Cather"));
+            this.Add(new Person("Isak", "Diena"));
+            this.Add(new Person("Victor", "Hugo"));
+        }
+    }
+}
